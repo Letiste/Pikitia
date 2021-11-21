facaa3c4139bd3c37162598d9ccd84e6818fd77a
Léo Salé <leo.sale72@gmail.com>
2021-11-21 15:43:19 +0000

    feat: can take and preview photos

---
+++ lib/screens/display_picture_screen.dart
@@ -0,0 +1,47 @@
+import 'dart:io';
+
+import 'package:flutter/material.dart';
+
+class DisplayPictureScreen extends StatelessWidget {
+  const DisplayPictureScreen({Key? key, required this.imagePath}) : super(key: key);
+
+  final String imagePath;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.black,
+      bottomNavigationBar: BottomAppBar(
+        color: Colors.black,
+        shape: const CircularNotchedRectangle(),
+        child: Column(
+          mainAxisSize: MainAxisSize.min,
+          children: [
+            Row(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                IconButton(
+                  color: Colors.white,
+                  iconSize: 36.0,
+                  icon: const Icon(
+                    Icons.cancel_outlined,
+                  ),
+                  onPressed: () => Navigator.of(context).pop(),
+                ),
+                IconButton(
+                  color: Colors.white,
+                  iconSize: 36.0,
+                  icon: const Icon(Icons.check_circle_outline_rounded),
+                  onPressed: () {},
+                ),
+              ],
+            ),
+            const SizedBox(height: 20),
+          ],
+        ),
+      ),
+      body: Image.file(File(imagePath), fit: BoxFit.contain),
+    );
+  }
+}
