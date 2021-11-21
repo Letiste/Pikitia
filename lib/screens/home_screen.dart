e64f22ee3009cb33e48859783c842ae02a57e941
Léo Salé <leo.sale72@gmail.com>
2021-11-21 10:13:49 +0000

    feat: rounded corners on bottom app bar

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:pikitia/widgets/photos_map.dart';
 
 class Home extends StatelessWidget {
   const Home({Key? key}) : super(key: key);
@@ -11,29 +12,37 @@ class Home extends StatelessWidget {
         child: const Icon(Icons.camera, size: 36.0),
         onPressed: () {},
       ),
-      bottomNavigationBar: BottomAppBar(
-        shape: const CircularNotchedRectangle(),
-        notchMargin: 4.0,
-        child: Row(
-          mainAxisSize: MainAxisSize.max,
-          mainAxisAlignment: MainAxisAlignment.spaceAround,
-          children: [
-            IconButton(
-              iconSize: 36.0,
-              icon: const Icon(
-                Icons.favorite,
-                color: Colors.red,
+      extendBody: true,
+      bottomNavigationBar: ClipRRect(
+        borderRadius: const BorderRadius.only(
+          topLeft: Radius.circular(30),
+          topRight: Radius.circular(30),
+        ),
+        child: BottomAppBar(
+          shape: const CircularNotchedRectangle(),
+          notchMargin: 3.0,
+          child: Row(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceAround,
+            children: [
+              IconButton(
+                iconSize: 36.0,
+                icon: const Icon(
+                  Icons.favorite,
+                  color: Colors.red,
+                ),
+                onPressed: () {},
               ),
-              onPressed: () {},
-            ),
-            IconButton(
-              iconSize: 36.0,
-              icon: const Icon(Icons.photo_size_select_actual_rounded),
-              onPressed: () {},
-            ),
-          ],
+              IconButton(
+                iconSize: 36.0,
+                icon: const Icon(Icons.photo_size_select_actual_rounded),
+                onPressed: () {},
+              ),
+            ],
+          ),
         ),
       ),
+      body: const PhotosMap(),
     );
   }
 }
