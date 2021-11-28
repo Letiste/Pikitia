21ee6d7ca2e703d74b09a8ac338f805ae67a8177
Léo Salé <leo.sale72@gmail.com>
2021-11-28 15:53:09 +0000

    feat: pikit form cardlike

--- lib/widgets/pikit_preview.dart
+++ lib/widgets/pikit_preview.dart
@@ -2,9 +2,12 @@ import 'package:flutter/material.dart';
 import 'package:pikitia/models/pikit.dart';
 
 class PikitPreview extends StatelessWidget {
-  const PikitPreview({required this.pikitImage, Key? key}) : super(key: key);
+  const PikitPreview({required this.pikitImage, required this.height, required this.width, Key? key}) : super(key: key);
 
   final PikitImage pikitImage;
+  final double height;
+  final double width;
+
   @override
   Widget build(BuildContext context) {
     return GestureDetector(
@@ -16,13 +19,58 @@ class PikitPreview extends StatelessWidget {
           ),
         ),
       ),
-      child: Container(
-          color: Colors.black,
-          padding: const EdgeInsets.all(2.0),
-          child: Hero(
-            tag: pikitImage,
-            child: Image.network(pikitImage.htmlUrlPreview),
-          )),
+      child: Stack(
+        children: [
+          ClipPath(
+            clipper: SignForm(),
+            child: Container(
+              color: Colors.blue,
+            ),
+          ),
+          Align(
+            alignment: Alignment.center,
+            child: Padding(
+              padding: const EdgeInsets.only(bottom: 8),
+              child: Container(
+                color: Colors.white,
+                padding: const EdgeInsets.fromLTRB(1.0, 1.0, 1.0, 1.0),
+                child: Hero(
+                  tag: pikitImage,
+                  child: Image.network(
+                    pikitImage.htmlUrlPreview,
+                    fit: BoxFit.scaleDown,
+                    height: height,
+                    width: width,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
+      ),
     );
+  }
+}
+
+class SignForm extends CustomClipper<Path> {
+  SignForm();
+
+  @override
+  Path getClip(Size size) {
+    Path path = Path();
+    path.lineTo(0, size.height - 8);
+    path.lineTo(size.width * .4, size.height - 8);
+    path.lineTo(size.width * .5, size.height); 
+    path.lineTo(size.width * .6, size.height - 8);
+    path.lineTo(size.width, size.height - 8);
+    path.lineTo(size.width, 0);
+
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(CustomClipper oldClipper) {
+    return oldClipper != this;
   }
 }
