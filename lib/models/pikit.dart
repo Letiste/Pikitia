3e2f2de724c7041fd003aef4cbc68d5af7e80bbf
Léo Salé <leo.sale72@gmail.com>
2021-11-28 11:09:00 +0000

    feat: use preview image

--- lib/models/pikit.dart
+++ lib/models/pikit.dart
@@ -1,8 +1,15 @@
 import 'package:geoflutterfire/geoflutterfire.dart';
 
 class Pikit {
-  Pikit({required this.htmlUrl, required this.position});
+  Pikit({required this.pikitImage, required this.position});
+
+  final PikitImage pikitImage;
+  final GeoFirePoint position;
+}
+
+class PikitImage {
+  PikitImage({required this.htmlUrl, required this.htmlUrlPreview});
 
   final String htmlUrl;
-  final GeoFirePoint position;
+  final String htmlUrlPreview;
 }
