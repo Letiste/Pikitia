075ab3571e07d1c7906654bbd554ef50b66febc8
Léo Salé <leo.sale72@gmail.com>
2021-12-02 18:25:25 +0000

    feat: can delete own pikits

--- lib/screens/user_pikits_screen.dart
+++ lib/screens/user_pikits_screen.dart
@@ -6,26 +6,73 @@ import '../locator.dart';
 
 class UserPikitsScreen extends StatelessWidget {
   UserPikitsScreen({Key? key}) : super(key: key) {
-    _pikitsFuture = locator<PikitService>().getUserPikits();
+    _pikitsStream = locator<PikitService>().getUserPikits();
   }
 
-  late final Future<List<Pikit>> _pikitsFuture;
+  late final Stream<List<Pikit>> _pikitsStream;
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<List<Pikit>>(
-      future: _pikitsFuture,
+    return StreamBuilder<List<Pikit>>(
+      stream: _pikitsStream,
       builder: (context, snapshot) {
         if (snapshot.hasData) {
           var pikits = snapshot.data!;
           return GridView.count(
             crossAxisCount: 2,
-            children: pikits.map((pikit) => Image.network(pikit.pikitImage.htmlUrl)).toList(),
+            children: pikits.map((pikit) => pikitImagePreview(context, pikit)).toList(),
           );
         } else {
-          return Center(child: CircularProgressIndicator());
+          return const Center(child: CircularProgressIndicator());
         }
       },
+    );
+  }
+
+  Widget pikitImagePreview(BuildContext context, Pikit pikit) {
+    return GestureDetector(
+      onTap: () => Navigator.of(context).push(MaterialPageRoute(builder: (context) => pikitImage(context, pikit))),
+      child: Image.network(pikit.pikitImage.htmlUrl),
+    );
+  }
+
+  Widget pikitImage(BuildContext context, Pikit pikit) {
+    return Stack(
+      children: [
+        Center(child: Image.network(pikit.pikitImage.htmlUrl)),
+        Align(
+          alignment: Alignment.bottomCenter,
+          child: Padding(
+            padding: const EdgeInsets.only(bottom: 20),
+            child: FloatingActionButton(
+              backgroundColor: Colors.white,
+              onPressed: () => showDialog(
+                context: context,
+                builder: (dialogContext) {
+                  return AlertDialog(
+                    title: const Text("Warning"),
+                    content: const Text("Are you sure you want to delete this Pikit?"),
+                    actions: [
+                      TextButton(onPressed: () => Navigator.of(dialogContext).pop(), child: const Text('Cancel')),
+                      TextButton(
+                          onPressed: () => locator<PikitService>().deletePikit(pikit).then((_) {
+                            Navigator.of(dialogContext).pop();
+                            Navigator.of(context).pop();
+                          }),
+                          child: const Text('Delete Pikit')),
+                    ],
+                  );
+                },
+              ),
+              child: const Icon(
+                Icons.delete,
+                color: Colors.red,
+                size: 36,
+              ),
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
